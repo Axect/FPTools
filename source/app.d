3bd09f6ff9c22273fccb89f257b4bb3259e7e14b
Axect <edeftg@gmail.com>
2018-09-12 14:30:52 +0900

    take & takeWhile

--- source/app.d
+++ source/app.d
@@ -7,8 +7,14 @@ void main() {
 	sum(1,2).writeln;
 	seq(1,10,2).writeln;
 	take(3, seq(1,10,2)).writeln;
+	takeWhile!int(x => x < 3, seq(1,10)).writeln;
+	f(2).writeln;
 }
 
 T sum(T) (T lhs, T rhs) {
 	return lhs + rhs;
+}
+
+bool f(T) (T x) {
+	return x < 3;
 }
--- source/dtools/fp.d
+++ source/dtools/fp.d
@@ -1,5 +1,22 @@
 module dtools.fp;
 
+/++
+    take - Usage : take(3, [1,2,3,4,5]) == [1,2,3]
++/
 T[] take(T) (int n, T[] list) {
     return list[0 .. n];
 }
+
+/++
+    takeWhile - Usage : takeWhile!int(x => x < 3, seq(1,10)) == [1,2]
++/
+T[] takeWhile(T) (bool delegate(T) p, T[] list) {
+    auto n = 0;
+    T[] result;
+    while(p(list[n])) {
+        result ~= list[n];
+        n++;
+    }
+    return result;
+}
+
